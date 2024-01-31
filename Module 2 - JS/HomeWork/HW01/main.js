35b391671fd541d5be35390528ffda4fd6878484
stoianova <stoianova.as@gmail.com>
2024-01-31 17:28:36 +0100

    module 2 - JS

---
+++ Module 2 - JS/HomeWork/HW01/main.js
@@ -0,0 +1,12 @@
+alert('Hello! It`s my first JS Homework');
+
+let userName = 'Sasha';
+let userAge = '24';
+let userScore = Number('80');
+
+userScore = 65; //для змінення значення 
+
+let firstPrompt = prompt('Ціна товару 100 грн, зараз діє знижка 40 грн. Яка вартість?');
+let secondPrompt = prompt('Скільки товарів Вам потрібно?');
+let summOperation = firstPrompt*secondPrompt;
+console.log((summOperation), 'До сплати:');
