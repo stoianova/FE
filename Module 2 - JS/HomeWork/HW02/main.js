c95134e5d0171bb245b6417f887d0141b865bca2
stoianova <stoianova.as@gmail.com>
2024-02-03 16:27:13 +0100

    не закінчене ДЗ 2

---
+++ Module 2 - JS/HomeWork/HW02/main.js
@@ -0,0 +1,62 @@
+// Порівняння чисел: За допомогою prompt отримайте два числа від користувача. Використовуючи if, else if, і else, виведіть у консоль, яке з них більше, менше, або вони рівні. Пам'ятайте про різницю між == і ===.
+
+
+let userNumber = Number(prompt('Обери число більше 10'));
+if(userNumber > 10){
+    console.log(`User chose ${userNumber} Great job`);
+}
+else{
+    console.log(`User chose ${userNumber} and its NOT correct`, prompt('Спробуй ще раз'));
+};
+
+let userSecondNumber = Number(prompt('Обери друге будь-яке число'));
+console.log(`User chose ${userSecondNumber}`); 
+
+console.log(`Compare ${userNumber}<${userSecondNumber}`); //як вивести рішення тру чи фолс в консолі? щось не виходить
+
+
+//Визначення року народження: Запитайте в користувача його вік за допомогою prompt. Використовуючи отримане значення та поточний рік (ви можете захардкодити поточний рік), виведіть у консоль рік народження користувача.
+
+let currentYear = 2024
+let userYear = Number(prompt(`Введи свій рік народження`));
+console.log(`Рік народження користувача ${userYear}`);
+let userAge = currentYear-userYear;
+console.log(userAge); 
+
+//Вгадай число: Запитайте у користувача число. Якщо введене число більше заданого вами (наприклад, 10), виведіть "Занадто велике". Якщо менше - "Занадто мале". Якщо користувач вгадав число, виведіть "Вітаємо, ви вгадали!".
+
+let myNumber = 22 || 23 || 24;
+let userGuessNumber = Number(prompt('Обери число від 20 до 25'));
+console.log(`User chose ${userGuessNumber}`)
+if(`${userGuessNumber === 22 || 23 || 24}` (alert('Вітаю, ви вгадали')));
+else{`${userGuessNumber === 20 || 21 || 25}` (alert('Нажаль, ви не вгадали'))}; 
+
+// Класифікація за оцінками: Запитайте у користувача його оцінку за 10-бальною шкалою за допомогою prompt. За допомогою if-else виведіть в консоль "Відмінно" (8-10), "Добре" (5-7), "Задовільно" (3-4), "Незадовільно" (менше 3).
+
+let userGrade = Number(prompt('Enter your grade'))
+if(`${userGrade=>8}`){
+    console.log(`User entered ${userGrade} 'Відмінно'`)};
+else if{`${userGrade == 7 || 6 || 5}`}{
+    console.log('Добре')};
+
+
+// Перепишіть використовуючи тернарний оператор
+// ---- Я зробила двома варіантами, не знаю який з них на більш вірному шляху
+let result = '';
+let score = 75;
+if (score > 50) {
+    result ="Пройшов";
+    } else {
+        result ="Не пройшов";
+    }
+let result = result>score ? result : console.log("Не пройшов");
+
+
+let tempResult = "";
+let temperature = 30;
+if (temperature > 25) {
+    tempResult ="Гаряче";
+} else {
+    tempResult ="Прохолодно";
+}
+let tempResult = temperature>25 ? tempResult : tempResult;
