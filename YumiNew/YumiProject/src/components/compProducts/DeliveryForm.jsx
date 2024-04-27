6979fd0aa7c50815b5f86cc3da10bd53ff5b0e84
stoianova <stoianova.as@gmail.com>
2024-04-27 22:58:42 +0200

    #

--- YumiNew/YumiProject/src/components/compProducts/DeliveryForm.jsx
+++ YumiNew/YumiProject/src/components/compProducts/DeliveryForm.jsx
@@ -1,31 +1,99 @@
 import React from 'react';
-// import './OrderTypeForm.css'
-// import './Cart.css'
-import OrderTypeForm from './OrderTypeForm';
 import './DeliveryForm.css'
+import {validName, validNumber, validStreet, validBuildNum, validApart} from './RegEx';
 
-function DeliveryForm(/* {setAway} */) {
+function DeliveryForm({setDelivery}) {
+
+    function firstFun(){
+        let name = document.querySelector ('#name');
+        let number = document.querySelector ('#number');
+        let street = document.querySelector ('#street');
+        let build = document.querySelector ('#build');
+        let apart = document.querySelector ('#apart');
+
+        if(validNumber.test(number.value) && validName.test(name.value) && validStreet.test(street.value) && validBuildNum.test(build.value) && validApart.test(apart.value)){
+            submitButton.removeAttribute('disabled')
+        }
+        else{
+            submitButton.setAttribute('disabled', true)
+        }   }
+
+
+        function secondFun(e){
+            e.preventDefault()
+            let name = document.querySelector ('#name');
+            let number = document.querySelector ('#number');
+            let street = document.querySelector ('#street');
+            let build = document.querySelector ('#build');
+            let apart = document.querySelector ('#apart');
+    
+
+            let newObj = {
+                nameNew: '',
+                numberNew: '',
+                streetNew: '',
+                buildNew: '',
+                apartNew: '',
+            }
+
+            newObj.nameNew = name.value
+            newObj.numberNew = number.value
+            newObj.streetNew = street.value
+            newObj.buildNew = build.value
+            newObj.apartNew = apart.value
+
+            localStorage.setItem('.order', JSON.stringify(newObj));
+
+            setTimeout(function another(){
+                let inputs = document.querySelectorAll('input')
+                for( let some of inputs){
+                    some.value = ''
+                }
+            }, 0)
+    }
+
     return (
         <>
-            <form action=''>
+        <div className="takeAwayForm">
+            <form action='' onChange={firstFun} onSubmit={secondFun}>
+                    <div className="orderText"> Your details for DELIVERY </div>
+
+                {/* <div className='biggerDelForm'> */}
+                <div className="innerFormDiv">
                 <div className='orderForm'>
+
  
                 <label htmlFor="name">Type your name</label>
-                <input type="text" name="name" id="name"/> <br />
-
-                <label htmlFor="email">Enter your email</label>
-                <input type="text" name="email" id="email"/> <br />
+                <input type="text" name="name" id="name"/> 
 
                 <label htmlFor="number">Enter your phone number</label>
-                <input type="number" name="number" id="number"/> <br />
+                <input type="number" name="number" id="number"/> 
 
-                <input type="submit" id="submitButton" value="Send" disabled /> <br />
+                <label htmlFor="street">Street</label>
+                <input type="text" name="street" id="street"/> 
 
-                <div className="btnBack" /* onClick={() => setForm(true)} */ >Back</div>
+                <label htmlFor="build">Building number</label>
+                <input type="text" name="build" id="build"/> 
+
+                <label htmlFor="apart">Apartment number</label>
+                <input type="number" name="apart" id="apart"/> 
+
+                <label htmlFor="comments">Comments:</label>
+                <input type="text" name="comments" id="comments"/> 
+                
                 </div>
+            </div>
+
+                <div className="takeBtns">
+
+                    <div className="btnBack" onClick={() => setDelivery(true)} >Back</div>
+                    <input type="submit" id="submitButton" value="Send" disabled /> 
+
+                </div>
+
             </form>
-            
-            <div id="alert"></div> 
+            <div id="alert"></div>
+            </div>
         </>
     );
 }
